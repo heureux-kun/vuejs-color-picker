56e58d7e51bbdc3885e4afdc52e41c685388896f
umlautone uenishi <uenishi@umlautone.jp>
2019-04-15 12:05:08 +0900

    座標系をクリックしてメインエリアの色を変更する処理を追加
    次はメインエリアをクリックしてRGBを取得できるようにする

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="ColorPicker">
-    <div id="brightnessSaturationSelect">
+    <div id="brightnessSaturationSelect" v-bind:style="colorPickerBackgroundColor">
 
     </div>
     <div id="ColorSelect" @click="getClickCoordinate">
 
     </div>
-    <div id="ColorResult">
+    <div id="ColorResult" @click="selectColor">
 
     </div>
-    {{ clickColorCoordinateY }}
+    clickColorCoordinateY:{{ clickColorCoordinateY }}<br />
+    degree:{{ degree }}<br />
+    {{ rColor }},{{ gColor }},{{ bColor }}
   </div>
 </template>
 
@@ -19,26 +21,50 @@ export default {
   data () {
     return {
       clickColorCoordinateY : '',
+      degree: '',
+      rColor: '',
+      gColor: '',
+      bColor: ''
     }
   },
   computed: {
+    colorPickerBackgroundColor: function(){
+      return 'background-color:rgb('+ this.rColor + ','+this.gColor + ','+ this.bColor + ')'
     }
   },
   methods: {
     getClickCoordinate: function(e){
       this.clickColorCoordinateY = e.offsetY
-    },
-    detectR: function(e){
-      let R
-      let clickColorCoordinate = e.offsetY
-      let degree = clickColorCoordinate / 300 * 360
-      if(degree < 60){
-        R = 255
-      }else if( 60 <= degre && degree < 120){
-        
-      }else if( 60 <= degre && degree < 120){
-        
+      this.degree = this.clickColorCoordinateY / 300 * 360
+      //R
+      if(this.degree < 60){
+        this.rColor = 255
+        this.gColor = Math.round( this.degree * (255 / 60) )
+        this.bColor = 0
+      }else if( 60 <= this.degree && this.degree < 120){
+        this.rColor = Math.round( 255 - ((this.degree - 60) * 255 / 60) )
+        this.gColor = 255
+        this.bColor = 0
+      }else if( 120 <= this.degree && this.degree < 180){
+        this.rColor = 0
+        this.gColor = 255
+        this.bColor = Math.round( (this.degree - 120) * 255 / 60 )
+      }else if( 180 <= this.degree && this.degree < 240){
+        this.rColor = 0
+        this.gColor = Math.round( 255 - (( this.degree-180 ) * 255 / 60) )
+        this.bColor = 255
+      }else if( 240 <= this.degree && this.degree < 300){
+        this.rColor = Math.round( (this.degree - 240) * 255 / 60 )
+        this.gColor = 0
+        this.bColor = Math.round( 255 - ((this.degree - 240) * 255 / 60) )
+      }else if( 300 <= this.degree && this.degree < 360){
+        this.rColor = 255
+        this.gColor = 0
+        this.bColor = 0
       }
+    },
+    selectColor: function(){
+      //ここから
     }
   }
 }
@@ -50,7 +76,6 @@ export default {
     width: 300px;
     height: 300px;
     background-image: linear-gradient(to bottom, transparent, #000000 ), linear-gradient(to right, #FFFFFF, transparent);
-    background-color: red;
   }
 
   #ColorSelect{
