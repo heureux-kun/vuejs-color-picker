249b5add25d5f8ed3748be43d2272e64f6bab83e
umlautone uenishi <uenishi@umlautone.jp>
2019-04-17 16:07:39 +0900

    クリックポイントによってHSVを算出できるようにした（RGBへの変換はまだ）

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="ColorPicker">
-    <div id="brightnessSaturationSelect" v-bind:style="colorPickerBackgroundColor">
+    <div id="SelectPoint" v-bind:style="pointPosition"></div>
+    <div id="BrightnessSaturationSelect" v-bind:style="colorPickerBackgroundColor" @click="getSv">
 
     </div>
+    H：{{h}}° S：{{s}}% V：{{v}}%　R：{{r}}　G：{{g}}　B：{{b}}
     <div id="ColorSelect" @click="getClickCoordinate">
 
     </div>
-    <div id="ColorResult" @click="selectColor">
-
+    <div id="ColorResult">
+      
     </div>
     clickColorCoordinateY:{{ clickColorCoordinateY }}<br />
     degree:{{ degree }}<br />
-    {{ rColor }},{{ gColor }},{{ bColor }}
+    rgb({{ rColor }},{{ gColor }},{{ bColor }})
   </div>
 </template>
 
@@ -21,21 +23,62 @@ export default {
   data () {
     return {
       clickColorCoordinateY : '',
-      degree: '',
-      rColor: '',
-      gColor: '',
-      bColor: ''
+      coordinateX: '',
+      coordinateY: '',
+      degree: '360',
+      rColor: '-',
+      gColor: '-',
+      bColor: '-',
+      h: '',
+      s: '0',
+      v: '100',
+      r: '-',
+      g: '-',
+      b: '-'
     }
   },
   computed: {
     colorPickerBackgroundColor: function(){
       return 'background-color:rgb('+ this.rColor + ','+this.gColor + ','+ this.bColor + ')'
+    },
+    pointPosition: function(){
+      let x = this.coordinateX - 6
+      let y = this.coordinateY + 6
+      return 'left: ' + x + 'px;'+'top: ' + y + 'px;'
+    },
+    hsvToRgb: function() {
+      let H = this.h
+      let S = this.s
+      let V = this.v
+      let C = V * S;
+      let Hp = H / 60;
+      let X = C * (1 - Math.abs(Hp % 2 - 1));
+
+      let R, G, B;
+      if (0 <= Hp && Hp < 1) {[R,G,B]=[C,X,0]};
+      if (1 <= Hp && Hp < 2) {[R,G,B]=[X,C,0]};
+      if (2 <= Hp && Hp < 3) {[R,G,B]=[0,C,X]};
+      if (3 <= Hp && Hp < 4) {[R,G,B]=[0,X,C]};
+      if (4 <= Hp && Hp < 5) {[R,G,B]=[X,0,C]};
+      if (5 <= Hp && Hp < 6) {[R,G,B]=[C,0,X]};
+
+      let m = V - C;
+      [R, G, B] = [R+m, G+m, B+m];
+
+      R = Math.floor(R * 255);
+      G = Math.floor(G * 255);
+      B = Math.floor(B * 255);
+
+      this.r = R
+      this.g = G
+      this.b = B
     }
   },
   methods: {
     getClickCoordinate: function(e){
       this.clickColorCoordinateY = e.offsetY
-      this.degree = this.clickColorCoordinateY / 300 * 360
+      this.degree = 360 - Math.floor(this.clickColorCoordinateY / 300 * 360)
+      this.h = this.degree
       //R
       if(this.degree < 60){
         this.rColor = 255
@@ -63,8 +106,11 @@ export default {
         this.bColor = 0
       }
     },
-    selectColor: function(){
-      //ここから
+    getSv: function(e){
+      this.coordinateX = e.offsetX
+      this.coordinateY = e.offsetY
+      this.s = Math.floor(this.coordinateX / 300 * 100)
+      this.v = 100 - Math.floor(this.coordinateY / 300 * 100)
     }
   }
 }
@@ -72,15 +118,22 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  #brightnessSaturationSelect{
+  #SelectPoint{
+    width: 12px;
+    height: 12px;
+    border-radius: 12px;
+    border:1px solid #fff;
+    position: relative;
+  }
+  #BrightnessSaturationSelect{
+    cursor: crosshair;
     width: 300px;
     height: 300px;
     background-image: linear-gradient(to bottom, transparent, #000000 ), linear-gradient(to right, #FFFFFF, transparent);
   }
-
   #ColorSelect{
     width: 100px;
     height: 300px;
-    background-image: linear-gradient( to bottom, rgb(255,0,0), rgb(255,255,0), rgb(0,255,0), rgb(0,255,255), rgb(0,0,255), rgb(255,0,255), rgb(255,0,0) );
+    background-image: linear-gradient( to top, rgb(255,0,0), rgb(255,255,0), rgb(0,255,0), rgb(0,255,255), rgb(0,0,255), rgb(255,0,255), rgb(255,0,0) );
   }
 </style>
